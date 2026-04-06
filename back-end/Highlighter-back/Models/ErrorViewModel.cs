2d469633475f68d01430c841f82422e83eeb785a
Focus2334 <danilakurzuev@gmail.com>
2026-04-06 15:13:52 +0500

    add back template

---
+++ back-end/Highlighter-back/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Highlighter_back.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
